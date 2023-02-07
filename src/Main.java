dfd9f4b62d30082167e778023c9cd9e1ec4a46b2
MirasZhumazhanov <200107017@stu.sdu.edu.kz>
2023-02-07 18:47:58 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
 
         System.out.println("Hello world");
         System.out.println("added folder");
+        System.out.println("napas lavandos flamateros dikobras i absos sosi ebasos");
+
     }
 }
